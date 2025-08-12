f105ae9127618bc171a686128fba24af31084d29
Zachary Davis <davisszachary@gmail.com>
2025-08-12 08:17:45 -0700

    Initial Spring Boot project setup

---
+++ src/main/java/com/zdavis1/springboottodo/SpringbootTodoApplication.java
@@ -0,0 +1,13 @@
+package com.zdavis1.springboottodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zdavis1/springboottodo/SpringbootTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdavis1.springboottodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

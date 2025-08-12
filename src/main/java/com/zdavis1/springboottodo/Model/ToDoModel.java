54e97feebc56282b311e6cfb9f1d7f34e0fad947
Zachary Davis <davisszachary@gmail.com>
2025-08-12 09:14:48 -0700

    Created ToDoModel entity class with fields and constructors

---
+++ src/main/java/com/zdavis1/springboottodo/Model/ToDoModel.java
@@ -0,0 +1,50 @@
+package com.zdavis1.springboottodo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ToDoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String dueDate;
+
+    public ToDoModel() {}
+
+    public ToDoModel (String title, String description, String dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+    public String getDueDate() {
+        return dueDate;
+    }
+}

d62a1846472b8fa3dfd67eb59fba56b872a30669
Zachary Davis <davisszachary@gmail.com>
2025-08-14 12:14:27 -0700

    Add ToDoService with full CRUD methods

---
+++ src/main/java/com/zdavis1/springboottodo/Service/ToDoService.java
@@ -0,0 +1,36 @@
+package com.zdavis1.springboottodo.Service;
+
+import com.zdavis1.springboottodo.Model.ToDoModel;
+import com.zdavis1.springboottodo.Repository.ToDoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class ToDoService {
+    private final ToDoRepository repository;
+
+    public ToDoService(ToDoRepository repository) {
+        this.repository = repository;
+    }
+
+    public ToDoModel createTask(ToDoModel task) {
+        return repository.save(task);
+    }
+
+    public List<ToDoModel> retrieveAllTasks() {
+        return repository.findAll();
+    }
+
+    public ToDoModel retriveByID(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deleteTask(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        }
+        else throw new NoSuchElementException("Task with id " + id + " not found");
+    }
+}

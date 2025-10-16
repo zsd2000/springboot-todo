80ee2f0330fd398c90d978d7072379b53685b16f
Zachary Davis <davisszachary@gmail.com>
2025-10-15 21:44:06 -0700

    Added Javadoc comments to Repository and Service layer.

--- src/main/java/com/zdavis1/springboottodo/Repository/ToDoRepository.java
+++ src/main/java/com/zdavis1/springboottodo/Repository/ToDoRepository.java
@@ -4,6 +4,11 @@ import com.zdavis1.springboottodo.Model.ToDoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository layer of ToDoApplication
+ * Uses an H2 (in-memory) database
+ * Handles data persistence and retrieval of ToDoModel entities
+ */
 @Repository
 public interface ToDoRepository extends JpaRepository<ToDoModel, Long> {
 }
--- src/main/java/com/zdavis1/springboottodo/Service/ToDoService.java
+++ src/main/java/com/zdavis1/springboottodo/Service/ToDoService.java
@@ -8,6 +8,11 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+/**
+ * Handles all business logic of tasks (ToDoModel entities)
+ * Interacts with the repository layer for CRUD operations
+ * Uses Dependency Injection to insert repository layer
+ */
 @Service
 public class ToDoService {
     private final ToDoRepository repository;
@@ -16,14 +21,29 @@ public class ToDoService {
         this.repository = repository;
     }
 
+    /**
+     * Creates a task
+     * @param task The task the user is adding to the database
+     * @return ToDoModel The task that was saved
+     */
     public ToDoModel createTask(ToDoModel task) {
         return repository.save(task);
     }
 
+    /**
+     * Retrieves all saved tasks in the database
+     * @return List<ToDoModel> A list of all saved tasks
+     */
     public List<ToDoModel> retrieveAllTasks() {
         return repository.findAll();
     }
 
+    /**
+     * Retrieves one task from the database
+     * The task is only retrieved if it exists within the database, throwing an exception if null
+     * @param id The unique ID assigned to the task
+     * @return Optional<ToDoModel> Either the specified task or an exception, if the task is not found
+     */
     public Optional<ToDoModel> retrieveByID(Long id) {
         Optional<ToDoModel> optional = repository.findById(id);
 
@@ -34,6 +54,12 @@ public class ToDoService {
         throw new NoSuchElementException();
     }
 
+    /**
+     * Updates a previously saved task
+     * @param id The unique ID assigned to the task
+     * @param task The updated task information
+     * @return Optional<ToDoModel> Either the updated task or an exception, if the desired task to be updated is not found
+     */
     public Optional<ToDoModel> updateTask(Long id, ToDoModel task) {
         Optional<ToDoModel> existingTask = retrieveByID(id);
 
@@ -48,6 +74,11 @@ public class ToDoService {
         throw new NoSuchElementException();
     }
 
+    /**
+     * Deletes the specified task
+     * @param id The unique ID assigned to the task
+     * @return Optional<ToDoModel> Either the deleted task or an exception, if the desired task to be deleted is not found
+     */
     public Optional<ToDoModel> deleteTask(Long id) {
         Optional<ToDoModel> task = retrieveByID(id);
 

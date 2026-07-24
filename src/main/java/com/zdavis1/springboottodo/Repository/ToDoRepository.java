9945cd3bdf1e28961afa372575f9203f2782e5cd
zsd2000 <davisszachary@gmail.com>
2026-07-24 16:55:13 -0700

    Utilize custom SQL query within the Sprind Data JPA

--- src/main/java/com/zdavis1/springboottodo/Repository/ToDoRepository.java
+++ src/main/java/com/zdavis1/springboottodo/Repository/ToDoRepository.java
@@ -2,7 +2,11 @@ package com.zdavis1.springboottodo.Repository;
 
 import com.zdavis1.springboottodo.Model.ToDoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
 
 /**
  * Repository layer of ToDoApplication
@@ -11,4 +15,15 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ToDoRepository extends JpaRepository<ToDoModel, Long> {
+
+    @Modifying
+    @Query
+            (value = "INSERT into TASKS(TITLE, DESCRIPTION, DUE_DATE, PRIORITY) VALUES" +
+            "(?, ?, ?, ?)",nativeQuery = true)
+    void insertTask(String title, String description, LocalDate dueDate, int priority);
+
+    // findByPriority
+    // findByCompleted
+    // updateTask
+    // deleteByID
 }
